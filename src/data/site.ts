3ced75dc7214214a4495e43b403819ce10fe7c44
Luigi Brandolini <luigi.brandolini@gmail.com>
2026-09-10 01:16:24 +0200

    Update Instagram profile link

--- src/data/site.ts
+++ src/data/site.ts
@@ -313,7 +313,7 @@ export const portfolio: Record<Locale, PortfolioItem[]> = {
 export const socials = {
   github: "https://github.com/luigibrandolini",
   facebook: "https://www.facebook.com/ErmesSystems",
-  instagram: "https://www.instagram.com/lb.creative.arts/",
+  instagram: "https://www.instagram.com/luigi.brandolini/",
   youtube: "https://www.youtube.com/@LuigiBrandolini",
   linkedin: "https://www.linkedin.com/in/luigibrandolini/",
 };
